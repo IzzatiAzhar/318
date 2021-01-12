c1e64fb234f25e2df15dffa4f9c51114619fad25
Izzati Azhar <izzatiazhar57@gmail.com>
2021-01-12 10:54:36 +0800

    12110

---
+++ charityhome/lostfoodproject.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>FoodForAll &mdash; We Care</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Overpass:300,400,500|Dosis:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+   <?php include 'header.php'; ?>
+    
+   <?php include 'navigation.php'; ?>
+  </nav>
+  <!-- END nav -->
+  
+  <div class="block-31" style="position: relative;">
+    <div class="owl-carousel loop-block-31 ">
+      <div class="block-30 block-30-sm item" style="background-image: url('images/banner.jpg');" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-7">
+              <h2 class="heading mb-5">The Lost Food Project Details</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+  <div class="site-section">
+    <div class="container">
+      <div class="row align-items-center mb-5">
+
+        <div class="col-md-7 order-md-2 mb-5 mb-md-0">
+          <img src="images/a2.jpg" alt="" class="img-fluid">
+        </div>
+
+        <div class="col-md-5 pr-md-5 mb-5 order-md-1">
+          <div class="block-41">
+            <div class="block-41-subheading d-flex">
+              <div class="block-41-number">01</div>
+            </div>
+            <h2 class="block-41-heading mb-3">We Support Each Other</h2>
+            <div class="block-41-text">
+              <p>By rescuing ‘lost’ food and finding it a new home with people who need it most.
+				 The Lost Food Project prevents quality, nutritious food and other surplus goods from going to landfill, and redistributes this food and other essential items
+				 to those in need – regardless of religion, gender, age, disability or ethnic group.
+				 Globally, more than 820 million people struggle with hunger every day. In Malaysia 3,000 tonnes of edible food is thrown away 
+				 each day which is enough to provide 10 million meals.<p>
+            </div>
+          </div>
+        </div>
+        
+      </div> <!-- .row -->
+
+      <div class="row align-items-center mb-5">
+        <div class="col-md-7 mb-5 mb-md-0">
+          <img src="images/chart.jpeg" alt="" class="img-fluid">
+        </div>
+
+        <div class="col-md-5 pl-md-5 mb-5">
+          <div class="block-41">
+            <div class="block-41-subheading d-flex">
+              <div class="block-41-number">02</div>
+            </div>
+            <h2 class="block-41-heading mb-3">Save The World</h2>
+            <div class="block-41-text">
+              <p>We rescue an average of 10 tonnes of good quality, surplus food per week from landfill. We are given this surplus food and 
+				other products by supermarkets, manufacturers and wholesale markets committed to reducing waste and feeding the less fortunate.
+				 With this rescued food we are able to provide an average of 33,000 meals a week for our charity partners and thousands
+				of Malaysian’s living on or below the poverty line.
+				</p>
+            </div>
+          </div>
+        </div>
+        
+      </div> <!-- .row -->
+
+    </div>
+  </div>
+
+ 
+
+  <footer class="footer">
+    <?php include 'footer.php'; ?>
+  </footer>
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
---
+++ charityhome/searchevents.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>FoodForAll &mdash; We Care</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Overpass:300,400,500|Dosis:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+	
+  <?php include 'navigation.php'; ?>
+  <!-- END nav -->
+  
+  <div class="block-31" style="position: relative;">
+    <div class="owl-carousel loop-block-31 ">
+      <div class="block-30 block-30-sm item" style="background-image: url('images/banner.jpg');" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-7">
+              <h2 class="heading mb-5">Find Out The Details Of The Events</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+  <div class="site-section section-counter">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 pr-5">
+          <div class="block-48">
+              <span class="block-48-text-1">Served Over</span>
+              <div class="block-48-counter ftco-number" data-number="1321901">0</div>
+              <span class="block-48-text-1 mb-4 d-block">Children in 150 Countries</span>
+              <p class="mb-0"><a href="#" class="btn btn-white px-3 py-2">View Our Program</a></p>
+            </div>
+        </div>
+        <div class="col-md-6 welcome-text">
+          <h2 class="display-4 mb-3">Who Are We?</h2>
+          <p class="lead">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+          <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia. </p>
+          <p class="mb-0"><a href="#" class="btn btn-primary px-3 py-2">Learn More</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="featured-section overlay-color-2" style="background-image: url('images/a2.jpg');">
+    
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6 mb-5 mb-md-0">
+          <img src="images/LOGO.jpg" alt="Image placeholder" class="img-fluid">
+        </div>
+
+        <div class="col-md-6 pl-md-5">
+
+          <div class="form-volunteer">
+            
+            <h2>Be A Volunteer Today</h2>
+			<h3>Searching the details of the events</h3><br>
+
+            <form action="searcheventsaction.php" id="form" method="GET">
+              <div class="form-group">
+                <!-- <label for="name">Name</label> -->
+                <input type="text" class="form-control py-2" name="searchevents" placeholder="Searching for ...">
+              </div>
+              <div class="form-group">
+                <input type="submit" class="btn btn-white px-5 py-2" value="Search">
+              </div>
+            </form>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+  </div> <!-- .featured-donate -->
+
+<footer class="footer">
+    <?php include 'footer.php'; ?>
+  </footer>
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+
+  <script src="js/jquery.fancybox.min.js"></script>
+  
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
